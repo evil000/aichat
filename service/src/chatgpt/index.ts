ae7a2894f820177154bd0877142a868b2e382445
GuangJian Chen <evil000@users.noreply.github.com>
2023-06-20 17:57:05 +0800

    Update index.ts

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -133,7 +133,7 @@ async function fetchUsage() {
 
   const API_BASE_URL = isNotEmptyString(OPENAI_API_BASE_URL)
     ? OPENAI_API_BASE_URL
-    : 'https://binsyai.openai.azure.com/'
+    : 'https://api.openai.com/'
 
   const [startDate, endDate] = formatDate()
 
